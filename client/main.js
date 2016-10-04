62aa2bfd044c70843b12d855dbfb655cd768ffa1
Mathieu Paradis <mparadis92@gmail.com>
2016-10-04 19:59:00 -0400

    second try again...

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating'; // import Template engine
+//import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html'; // importation du template
+
+Template.content.events({ // event du template content
+	"click .content .btn":function(){ // click button
+		
+		
+	}
+});
